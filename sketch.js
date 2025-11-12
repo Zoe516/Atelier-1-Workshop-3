4983ed1fa09307face23ea99e9f4491748118a32
Zoe Langille <zlangille@ocadu.ca>
2025-11-12 15:56:12 -0500

    Frustration parameter complete

--- sketch.js
+++ sketch.js
@@ -5,11 +5,13 @@ let playState = 0;
 let waterRefill;
 let water = 0;
 let frustration = 0;
+let angryBottle;
 
 function preload(){
-  Bottle = loadImage('images/BOTTLE.01.png',0,0);
+  Bottle = loadImage('images/BOTTLE.01.png');
   BottleGif = loadImage('images/bottle.gif');
   waterRefill = loadImage('images/waterrefill.gif')
+  angryBottle = loadImage('images/angry_bottle.gif')
 }
 
 function setup() {
@@ -36,24 +38,37 @@ function draw() {
     playState = 4
   }
   playState = playState - 1
+  if (frustration >= 75){
+    image(angryBottle,0,0)
+  }
+  if (frustration >= 100){
+    frustration = 100
+  }
+  else if (frustration <= 0){
+    frustration = 0
+  }
   if (water == 1){
     image(waterRefill,0,0)
   }
-  if (frustration == 10){
-    //some kind of frustration interaction maybe a colour filter to show red anger. 
+  if (frustration <= 74){
+    textSize(32);
+    fill(255);
+    stroke(0);
+    strokeWeight(4);
+    text('Dont shake the bottle!', 15, 425);
+  }
+  if (frustration >= 75){
+    textSize(32);
+    fill(255);
+    stroke(0);
+    strokeWeight(4);
+    text(':(, now tap to make her feel better.', 15, 425);
   }
 } 
+
 function deviceShaken() {
   if (window.sensorsEnabled) {
     playState = playState + 2;
     frustration = frustration + 1
   }
-}
-function touchStarted(){
-  if (window.sensorsEnabled){
-    water = 1 
-  }
-}
-function touchEnded(){
-  water = 0
 }
